5759a2ff46fde0ca2b874f86b29529493522f323
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-09 01:08:00 -0300

    [Davi] Applying Authorization to a Set of Routes

--- api/src/routes/sales.routes.js
+++ api/src/routes/sales.routes.js
@@ -2,11 +2,17 @@ const { Router } = require("express");
 const SalesController = require("../controllers/SalesController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
+// Importamos o middleware responsavel pela autorização do tipo de usuário
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization");
+
 const salesRoutes = Router();
 
 const salesController = new SalesController();
 
 salesRoutes.use(ensureAuthenticated);
+
+// Utilizamos o middleware para todas as rotas, onde podemos colocar qual perfil tem acesso
+salesRoutes.use(verifyUserAuthorization("admin"));
 
 salesRoutes.get("/", salesController.index);
 
