dba66abfd87dad7bca2407057cf0f28aff7929d2
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-09 01:25:36 -0300

    [Davi] Applying Authorization to Specific Routes

--- api/src/routes/products.routes.js
+++ api/src/routes/products.routes.js
@@ -2,6 +2,9 @@ const { Router } = require("express");
 const ProductsController = require("../controllers/ProductsController");
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
+// Importamos o middleware responsavel pela autorização do tipo de usuário
+const verifyUserAuthorization = require("../middlewares/verifyUserAuthorization");
+
 const productsRoutes = Router();
 
 const productsController = new ProductsController();
@@ -9,6 +12,8 @@ const productsController = new ProductsController();
 productsRoutes.use(ensureAuthenticated);
 
 productsRoutes.get("/", productsController.index);
-productsRoutes.post("/", productsController.create);
+
+// Somente o perfil de admin pode acessar e criar novos produtos
+productsRoutes.post("/", verifyUserAuthorization("admin"), productsController.create);
 
 module.exports = productsRoutes;
