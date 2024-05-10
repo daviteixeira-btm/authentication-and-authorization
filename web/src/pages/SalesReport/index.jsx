81acb31252666705342a3516999cfd6ee1a5642e
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-10 00:51:46 -0300

    [Davi] Using Cookie in Authorization

--- web/src/pages/SalesReport/index.jsx
+++ web/src/pages/SalesReport/index.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
+import { api } from "../../services/api";
 
 export function SalesReport() {
+
+  useEffect(() => {
+    /* Com a propriedade x conseguimos usar o cookie incerido na requisição 
+    para estabelecer a comunicação com o backend */
+    api.get("/sales", { withCredentials: true})
+      .then((response) => console.log(response.data));
+  }, [])
+
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
