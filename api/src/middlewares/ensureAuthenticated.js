a1a1852002fa28640f33052e87de9f651fa349a3
Davi Teixeira <daviteixeira.btm@gmail.com>
2024-05-10 00:51:30 -0300

    [Davi] Using Cookie in Authorization

--- api/src/middlewares/ensureAuthenticated.js
+++ api/src/middlewares/ensureAuthenticated.js
@@ -10,7 +10,7 @@ function ensureAuthenticated(request, response, next) {
     throw new AppError('JWT token não informado', 401);
   }
 
-  const [, token] = authHeader.split('token=');
+  const [, token] = authHeader.cookie.split('token=');
 
   try {
     // Recuperamos a informação de 'role' do usuário
